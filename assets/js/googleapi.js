c8868bf0129448f5a7caaa0001522e1afd6fbd14
AziiQ <120739025+AziiQ@users.noreply.github.com>
2023-02-16 11:29:46 +0000

    Add files via upload

---
+++ assets/js/googleapi.js
@@ -0,0 +1,36 @@
+
+
+  /**
+   * Sample JavaScript code for calendar.calendarList.get
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/code-samples#javascript
+   */
+
+  function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/calendar.events.readonly"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyAM0yPRxm2z4np7lH4gdhwMwwN-nWLx71Y");
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.calendar.calendarList.get({
+      "calendarId": "d54d459670e39a9f2ef7d90ae2fbdefffe87e7498a80433ffc9c03339bc31ca1@group.calendar.google.com"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "d54d459670e39a9f2ef7d90ae2fbdefffe87e7498a80433ffc9c03339bc31ca1@group.calendar.google.com"});
+  });
+
+
